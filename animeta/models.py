cd9aa633bd5be596d70a5e64a156a811c6470233
Taeho Kim <dittos@gmail.com>
2013-09-13 22:15:53 +0900

    Implement status/progress updating. Clarify ORM declarations.

--- animeta/models.py
+++ animeta/models.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+import datetime
+import pytz
 from django.conf import settings; settings.configure(); del settings
 from django.contrib.auth.hashers import check_password
+from sqlalchemy import event
 from flask.ext.login import UserMixin
 from animeta import db
 
@@ -17,25 +21,25 @@ class Work(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Unicode(100), nullable=False, index=True)
 
-class LibraryItem(db.Model):
-    STATUS_FINISHED = 0
-    STATUS_WATCHING = 1
-    STATUS_SUSPENDED = 2
-    STATUS_INTERESTED = 3
+class StatusType(db.TypeDecorator):
+    impl = db.Integer
 
-    __tablename__ = 'record_record'
-    __table_args__ = (db.UniqueConstraint('user_id', 'work_id'), )
+    NONE = ''
+    FINISHED = 'finished'
+    WATCHING = 'watching'
+    SUSPENDED = 'suspended'
+    INTERESTED = 'interested'
+    table = (FINISHED, WATCHING, SUSPENDED, INTERESTED)
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
-    work_id = db.Column(db.Integer, db.ForeignKey(Work.id), nullable=False)
-    title = db.Column(db.Unicode(100), nullable=False)
-    progress = db.Column('status', db.Unicode(30))
-    status = db.Column('status_type', db.Integer, nullable=False, default=STATUS_WATCHING)
-    updated_at = db.Column(db.DateTime(timezone=True))
+    def process_bind_param(self, value, dialect):
+        if value == self.NONE:
+            return -1
+        return self.table.index(value)
 
-    user = db.relationship(User, backref=db.backref('library_items', lazy='dynamic'))
-    work = db.relationship(Work, backref=db.backref('library_items', lazy='dynamic'))
+    def process_result_value(self, value, dialect):
+        if value == -1:
+            return self.NONE
+        return self.table[value]
 
 class Update(db.Model):
     __tablename__ = 'record_history'
@@ -44,14 +48,58 @@ class Update(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     work_id = db.Column(db.Integer, db.ForeignKey(Work.id), nullable=False)
     progress = db.Column('status', db.Unicode(30))
-    status = db.Column('status_type', db.Integer, nullable=False,
-                        default=LibraryItem.STATUS_WATCHING)
     updated_at = db.Column(db.DateTime(timezone=True))
     comment = db.Column(db.UnicodeText, nullable=False)
 
     user = db.relationship(User, backref=db.backref('updates', lazy='dynamic'))
     work = db.relationship(Work, backref=db.backref('updates', lazy='dynamic'))
-    library_item = db.relationship(LibraryItem, primaryjoin=db.and_(
-        LibraryItem.user_id == db.foreign(user_id),
-        LibraryItem.work_id == db.foreign(work_id)
-    ), backref=db.backref('updates', lazy='dynamic'))
+
+    def __init__(self, **kwargs):
+        self.updated_at = datetime.datetime.now(pytz.utc)
+        super(Update, self).__init__(**kwargs)
+
+    # Status column in an Update is not used anymore. Read-only.
+    _status = db.Column('status_type', StatusType, nullable=False, default=StatusType.NONE)
+
+    @property
+    def status(self):
+        return self._status
+
+class LibraryItem(db.Model):
+    __tablename__ = 'record_record'
+    __table_args__ = (db.UniqueConstraint('user_id', 'work_id'), )
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    work_id = db.Column(db.Integer, db.ForeignKey(Work.id), nullable=False)
+    title = db.Column(db.Unicode(100), nullable=False)
+    status = db.Column('status_type', StatusType, nullable=False, default=StatusType.WATCHING)
+
+    # Denormalized columns
+    progress = db.Column('status', db.Unicode(30))
+    updated_at = db.Column(db.DateTime(timezone=True))
+
+    user = db.relationship(User, backref=db.backref('library_items', lazy='dynamic'))
+    work = db.relationship(Work, backref=db.backref('library_items', lazy='dynamic'))
+    updates = db.relationship(Update, primaryjoin=db.and_(
+        Update.user_id == db.foreign(user_id),
+        Update.work_id == db.foreign(work_id)
+    ), lazy='dynamic', uselist=True, backref=db.backref('library_item', uselist=False))
+
+@event.listens_for(LibraryItem.updates, 'append')
+def append_update(item, update, initiator):
+    # Update denormalized columns
+    item.progress = update.progress
+    item.updated_at = update.updated_at
+
+    # Assign relationship
+    update.user_id = item.user_id
+    update.work_id = item.work_id
+
+@event.listens_for(LibraryItem.updates, 'remove')
+def remove_update(item, update, initiator):
+    last_updates = item.updates.order_by(Update.id.desc())[:2]
+    if len(last_updates) < 2:
+        item.progress = ''
+    else:
+        item.progress = last_updates[1].progress
--- animeta/views/library.py
+++ animeta/views/library.py
@@ -1,5 +1,6 @@
 import flask
-from animeta import models
+from flask.ext.login import current_user, login_required
+from animeta import db, models
 
 bp = flask.Blueprint('library', __name__)
 
@@ -20,3 +21,27 @@ def item_detail(id):
         item=item,
         updates=item.updates.order_by(models.Update.id.desc()),
     )
+
+@bp.route('/records/<id>/', methods=['POST'])
+@login_required
+def update_item(id):
+    item = models.LibraryItem.query.get_or_404(id)
+    if item.user != current_user:
+        flask.abort(403)
+    item.status = flask.request.form['status']
+    db.session.commit()
+    return flask.redirect(flask.url_for('.item_detail', id=item.id))
+
+@bp.route('/records/<id>/updates', methods=['POST'])
+@login_required
+def create_update(id):
+    item = models.LibraryItem.query.get_or_404(id)
+    if item.user != current_user:
+        flask.abort(403)
+    update = models.Update(
+        progress=flask.request.form['progress'],
+        comment=flask.request.form['comment'],
+    )
+    item.updates.append(update)
+    db.session.commit()
+    return flask.redirect(flask.url_for('.item_detail', id=item.id))
